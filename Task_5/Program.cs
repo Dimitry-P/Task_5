998e679659ae62ab80c26902c7c890918522ef7c
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:26:22 +0300

    Add project files.

---
+++ Task_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        //•	Задача 5: Напишите программу, которая
+        //выводит таблицу умножения от 1 до 10 с использованием циклов.
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <11; i++)
+            {
+                for(int j = 1; j < 11; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + i * j);
+                }
+            }
+        }
+    }
+}
